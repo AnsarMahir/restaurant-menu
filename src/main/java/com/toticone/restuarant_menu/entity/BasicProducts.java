71b0656dfe59c2ac675610a84a7e58e94f100430
Ansak <ansak@vizumatix.com>
2025-10-05 12:44:11 +0530

    Initial commit

---
+++ src/main/java/com/toticone/restuarant_menu/entity/BasicProducts.java
@@ -0,0 +1,4 @@
+package com.toticone.restuarant_menu.entity;
+
+public class BasicProducts {
+}
---
+++ src/test/java/com/toticone/restuarant_menu/RestuarantMenuApplicationTests.java
@@ -0,0 +1,13 @@
+package com.toticone.restuarant_menu;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestuarantMenuApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
