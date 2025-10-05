d3e9e5f27dbc807bb8c1fc60a76c5461c7ecc1f9
Ansak <mohamedansak@gmail.com>
2025-10-05 12:44:11 +0530

    Initial commit

---
+++ src/main/java/com/toticone/restuarant_menu/RestuarantMenuApplication.java
@@ -0,0 +1,13 @@
+package com.toticone.restuarant_menu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestuarantMenuApplication {
+
+    public static void main(String[] args) {
+		SpringApplication.run(RestuarantMenuApplication.class, args);
+	}
+
+}
